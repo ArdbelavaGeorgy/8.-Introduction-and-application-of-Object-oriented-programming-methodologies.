661ee5bf44d14ce7857a1a643302e9031dd966a1
ArdbelavaGeorgy <147245423+ArdbelavaGeorgy@users.noreply.github.com>
2023-12-08 15:10:40 +0300

    Добавьте файлы проекта.

---
+++ Dining room/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+
+// Базовый класс Еда
+class Food
+{
+    public string Name { get; set; }
+
+    public Food(string name)
+    {
+        Name = name;
+    }
+
+    public virtual void Prepare()
+    {
+        Console.WriteLine($"\"{Name}\" готовится.");
+    }
+}
+
+// Производный класс Бутерброд
+class Sandwich : Food
+{
+    public Sandwich(string name) : base(name) { }
+
+    public override void Prepare()
+    {
+        Console.WriteLine($"Бутерброд \"{Name}\" собирается.");
+    }
+}
+
+// Производный класс Пицца
+class Pizza : Food
+{
+    public Pizza(string name) : base(name) { }
+
+    public override void Prepare()
+    {
+        Console.WriteLine($"Пицца \"{Name}\" готовится в печи.");
+    }
+}
+
+// Производный класс Блины
+class Pancake : Food
+{
+    public Pancake(string name) : base(name) { }
+
+    public override void Prepare()
+    {
+        Console.WriteLine($"Блины \"{Name}\" готовятся на сковороде.");
+    }
+}
+
+// Производный класс Десерт Тирамису
+class Tiramisu : Food
+{
+    public Tiramisu(string name) : base(name) { }
+
+    public override void Prepare()
+    {
+        Console.WriteLine($"Десерт Тирамису \"{Name}\" готовится.");
+    }
+}
+
+// Производный класс Стейк
+class Steak : Food
+{
+    public Steak(string name) : base(name) { }
+
+    public override void Prepare()
+    {
+        Console.WriteLine($"Стейк \"{Name}\" жарится.");
+    }
+}
+
+// Производный класс Салат Цезарь
+class CaesarSalad : Food
+{
+    public CaesarSalad(string name) : base(name) { }
+
+    public override void Prepare()
+    {
+        Console.WriteLine($"Салат Цезарь \"{Name}\" нарезается и заправляется.");
+    }
+}
+
+// Производный класс Борщ
+class Borsch : Food
+{
+    public Borsch(string name) : base(name) { }
+
+    public override void Prepare()
+    {
+        Console.WriteLine($"Борщ \"{Name}\" варится.");
+    }
+}
+
+// Производный класс Паста
+class Pasta : Food
+{
+    public Pasta(string name) : base(name) { }
+
+    public override void Prepare()
+    {
+        Console.WriteLine($"Паста \"{Name}\" готовится.");
+    }
+}
+
+// Производный класс Рыба
+class Fish : Food
+{
+    public Fish(string name) : base(name) { }
+
+    public override void Prepare()
+    {
+        Console.WriteLine($"Рыба \"{Name}\" жарится.");
+    }
+}
+
+// Производный класс Курица
+class Chicken : Food
+{
+    public Chicken(string name) : base(name) { }
+
+    public override void Prepare()
+    {
+        Console.WriteLine($"Курица \"{Name}\" запекается.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Создание объектов различных классов еды
+        Food[] dishes = new Food[]
+        {
+            new Sandwich("С ветчиной и сыром"),
+            new Pizza("Пепперони"),
+            new Pancake("С икрой"),
+            new Tiramisu("Классический"),
+            new Steak("Рибай"),
+            new CaesarSalad("С курицей"),
+            new Borsch("Классический"),
+            new Pasta("Карбонара"),
+            new Fish("Лосось"),
+            new Chicken("Гриль")
+        };
+
+        // Подготовка каждого блюда
+        foreach (var dish in dishes)
+        {
+            dish.Prepare();
+        }
+    }
+}
---
+++ GTA-5/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+
+// Базовый класс Транспорт
+class Transport
+{
+    public string Model { get; set; }
+
+    public Transport(string model)
+    {
+        Model = model;
+    }
+
+    public virtual void StartEngine()
+    {
+        Console.WriteLine($"Двигатель транспорта \"{Model}\" запущен.");
+    }
+}
+
+// Производный класс Танк
+class Tank : Transport
+{
+    public string Division { get; set; }
+
+    public Tank(string model, string division) : base(model)
+    {
+        Division = division;
+    }
+
+    public override void StartEngine()
+    {
+        Console.WriteLine($"Двигатель танка \"{Division} {Model}\" запущен.");
+    }
+}
+
+// Производный класс Самолет
+class Airplane : Transport
+{
+    public string Airline { get; set; }
+
+    public Airplane(string model, string airline) : base(model)
+    {
+        Airline = airline;
+    }
+
+    public override void StartEngine()
+    {
+        Console.WriteLine($"Двигатели самолета \"{Airline} {Model}\" запущены.");
+    }
+}
+
+// Производный класс Вертолет
+class Helicopter : Transport
+{
+    public string Manufacturer { get; set; }
+
+    public Helicopter(string model, string manufacturer) : base(model)
+    {
+        Manufacturer = manufacturer;
+    }
+
+    public override void StartEngine()
+    {
+        Console.WriteLine($"Двигатель вертолета \"{Manufacturer} {Model}\" запущен.");
+    }
+}
+
+// Производный класс Автобус
+class Bus : Transport
+{
+    public string Route { get; set; }
+
+    public Bus(string model, string route) : base(model)
+    {
+        Route = route;
+    }
+
+    public override void StartEngine()
+    {
+        Console.WriteLine($"Двигатель автобуса \"{Route} {Model}\" запущен.");
+    }
+}
+
+// Производный класс Велосипед
+class Bicycle : Transport
+{
+    public string Type { get; set; }
+
+    public Bicycle(string model, string type) : base(model)
+    {
+        Type = type;
+    }
+
+    // Велосипед не имеет двигателя, поэтому метод переопределен
+    public override void StartEngine()
+    {
+        Console.WriteLine($"Велосипед \"{Type} {Model}\" готов к поездке.");
+    }
+}
+
+// Производный класс Водный мотоцикл
+class JetSki : Transport
+{
+    public string Make { get; set; }
+
+    public JetSki(string model, string make) : base(model)
+    {
+        Make = make;
+    }
+
+    public override void StartEngine()
+    {
+        Console.WriteLine($"Двигатель водного мотоцикла \"{Make} {Model}\" запущен.");
+    }
+}
+
+// Производный класс Грязевой мотоцикл
+class DirtBike : Transport
+{
+    public string Type { get; set; }
+
+    public DirtBike(string model, string type) : base(model)
+    {
+        Type = type;
+    }
+
+    public override void StartEngine()
+    {
+        Console.WriteLine($"Двигатель грязевого мотоцикла \"{Type} {Model}\" запущен.");
+    }
+}
+
+// Производный класс Спортивный автомобиль
+class SportsCar : Transport
+{
+    public string Brand { get; set; }
+
+    public SportsCar(string model, string brand) : base(model)
+    {
+        Brand = brand;
+    }
+
+    public override void StartEngine()
+    {
+        Console.WriteLine($"Двигатель спортивного автомобиля \"{Brand} {Model}\" запущен.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Создание объектов различных классов транспорта
+        Transport[] vehicles = new Transport[]
+        {
+            new Tank("Khanjali", "Warstock"),
+            new Airplane("Boeing 747", "Delta"),
+            new Helicopter("Apache", "Boeing"),
+            new Bus("City Bus", "Route 66"),
+            new Bicycle("Mountain Bike", "Off-Road"),
+            new JetSki("Seashark", "Speedophile"),
+            new DirtBike("BF400", "Nagasaki"),
+            new SportsCar("Zentorno", "Grotti"),
+        };
+
+        // Запуск двигателей (или подготовка к поездке для велосипеда) каждого транспортного средства
+        foreach (var vehicle in vehicles)
+        {
+            vehicle.StartEngine();
+        }
+    }
+}
